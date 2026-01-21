18884689803c1d2d05088326cd658a5fb7092380
HanaFatihaHadzri <122062870+HanaFatihaHadzri@users.noreply.github.com>
2026-01-21 22:28:50 +0800

    Add code to all files

---
+++ script.js
@@ -0,0 +1,12 @@
+const catImage = document.getElementById("catImage");
+const nextBtn = document.getElementById("nextBtn");
+
+function loadCat(){
+    const url = "https://cataas.com/cat";
+    catImage.src = url;
+}
+
+nextBtn.addEventListener("click", loadCat);
+
+//load first cat when page opens
+loadCat();
